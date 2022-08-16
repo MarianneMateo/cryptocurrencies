51b1d9d8f6b9c59eed473dbfe94a3c8ee48b6978
Marianne Mateo <mariannejmb18@gmail.com>
2022-08-16 17:03:42 -0400

    done!

--- src/components/Crypto.jsx
+++ src/components/Crypto.jsx
@@ -5,14 +5,17 @@ const Crypto = ({ cryptocurrencies }) => {
 		<div className="row row-cols-md-4">
 			{cryptocurrencies.map((e, i) => (
 				<div key={i} className="col mb-5">
-					<div className="card" style={{ minWidth: '200px' }}>
-						<img src={e.image} />
+					<div className="card" style={{ width: '260px'}}>
+						<img src={e.image} style={{ width:'35px',  marginLeft: '40%' }} />
 						<div className="card-body">
 							<h5 className="card-title">{e.name}</h5>
-							<hr />
 							<p>{e.symbol.toUpperCase()}</p>
+							<hr />
+							<label style={{ color: '#a9a2a2' }}>Current Price</label>
 							<p>${e.current_price}</p>
+							<label style={{ color: '#a9a2a2' }}>Volume</label>
 							<p>${e.total_volume.toLocaleString()}</p>
+							<label style={{ color: '#a9a2a2' }}>% Change price 24h</label>
 							{e.price_change_percentage_24h < 0 ? (
 								<p className="red">
 									{e.price_change_percentage_24h.toFixed(2)}%
@@ -22,7 +25,8 @@ const Crypto = ({ cryptocurrencies }) => {
 									{e.price_change_percentage_24h.toFixed(2)}%
 								</p>
 							)}
-							<p>Mkt Cap: ${e.market_cap.toLocaleString()}</p>
+							<label style={{ color: '#a9a2a2' }}>Mkt Cap</label>
+							<p>${e.market_cap.toLocaleString()}</p>
 						</div>
 					</div>
 				</div>
